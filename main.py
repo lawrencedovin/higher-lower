6f310927a48cd68bd26828187fce8d90f764a89e
Lawrence Dovin <lawrencedovin@gmail.com>
2021-03-08 19:54:14 -0500

    Base game functions

--- main.py
+++ main.py
@@ -2,20 +2,31 @@ from art import logo, vs
 from game_data import data
 from random import randint
 
-data_length = len(data) - 1
-
-player_a = data[randint(0, data_length)]
-player_b = data[randint(0, data_length)]
-score = 0
-
 def print_player(player):
     return f"{player['name']}, {player['description']}, from {player['country']}"
 
-def play_game(player_a, player_b):
+def play_game(score):
+    data_length = len(data) - 1
+    player_a = data[randint(0, data_length)]
+    player_b = data[randint(0, data_length)]
+
     print(logo)
-    print(f'Current score: {score}')
+    if score > 0: print(f"You're right! Current score: {score}")
     print(f'Compare A: {print_player(player_a)}')
     print(vs)
     print(f'Against B: {print_player(player_b)}')
+    guess = input("Who has more followers? Type 'A' or 'B': ").upper()
+    if guess == 'A': 
+        if player_a['follower_count'] > player_b['follower_count']:
+            score += 1
+            play_game(score)
+        else:
+            print('Incorrect game over')
+    elif guess == 'B':
+        if player_b['follower_count'] > player_a['follower_count']:
+            score += 1
+            play_game(score)
+        else:
+            print('Incorrect game over')
 
-play_game(player_a, player_b)
+play_game(0)
